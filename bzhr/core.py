237b7b7373967e038cce9b0ab03fee9cf3ed9763
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-11 23:59:39 -0500

    Create s3 module.

--- bzhr/core.py
+++ bzhr/core.py
@@ -8,6 +8,7 @@ and processes new tasks to be run in response.
 import pugsql
 
 from .config import Config, getenv
+from .s3 import S3
 from .wikidot import Wikidot
 
 
@@ -16,14 +17,17 @@ class BackupDispatcher:
         "config",
         "wikidot",
         "database",
+        "s3",
     )
 
     config: Config
     wikidot: Wikidot
+    s3: S3
 
     def __init__(self, config):
         self.config = config
         self.wikidot = Wikidot()
+        self.s3 = S3(config)
         self.database = pugsql.module("queries/")
         self.database.connect(getenv("POSTGRES_DATABASE_URL"))
 
---
+++ bzhr/s3.py
@@ -0,0 +1,52 @@
+"""
+Utilities for interacting with the S3 bucket where Wikidot files are being kept.
+"""
+
+import hashlib
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+
+from .config import Config, getenv
+
+FILE_DIRECTORY = "files"
+
+
+class S3:
+    __slots__ = (
+        "bucket",
+        "client",
+    )
+
+    bucket: str
+
+    def __init__(self, config: Config):
+        self.bucket = config.s3_bucket
+        self.client = boto3.client(
+            "s3",
+            aws_access_key_id=getenv("S3_ACCESS_KEY"),
+            aws_secret_access_key=getenv("S3_SECRET_KEY"),
+        )
+
+    def upload_file(self, blob: bytes):
+        blob_hash = hashlib.sha512(blob).hexdigest()
+        path = os.path.join(FILE_DIRECTORY, blob_hash)
+
+        # Only upload if not present
+        if self.object_exists(path):
+            self.client.put_object(
+                Bucket=self.bucket,
+                Path=path,
+                Body=blob,
+            )
+
+    def object_exists(self, path) -> bool:
+        try:
+            self.client.head_object(
+                Bucket=self.bucket,
+                Key=path,
+            )
+            return True
+        except ClientError:
+            return False
