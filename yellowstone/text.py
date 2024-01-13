586d9bf45b32e4cfc5d29adac88ca178c8d8a3de
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-13 13:54:45 -0500

    Add upsert function for text in module.

--- yellowstone/text.py
+++ yellowstone/text.py
@@ -1,9 +1,16 @@
-# TODO demo of text hashing
+"""
+Utility to manage text fields in the database.
+"""
 
 from Crypto.Hash import KangarooTwelve
 
 
-def hash_text(value: str) -> bytes:
-    h = KangarooTwelve.new()
-    h.update(value.encode("utf-8"))
-    return h.read(16)
+def hash_text(contents: str) -> bytes:
+    hash = KangarooTwelve.new()
+    hash.update(contents.encode("utf-8"))
+    return hash.read(16)
+
+
+def add_text(contents: str, *, database) -> None:
+    hash = hash_text(contents)
+    database.add_text(hash=hash, contents=contents)
