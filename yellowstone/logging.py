2b4ffe306e3b136b63e9313d7bc76ba79d9f1ea1
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-13 18:01:20 -0500

    Change log formatting to add dates to log file.

--- yellowstone/logging.py
+++ yellowstone/logging.py
@@ -8,16 +8,16 @@ import sys
 LOG_FILE = f"{__package__}.log"
 LOG_FILE_MODE = "w"
 LOG_FORMAT = "[%(levelname)s] %(name)s: %(message)s"
+LOG_FILE_FORMAT = f"%(asctime)s {LOG_FORMAT}"
+LOG_DATE_FORMAT = "%Y/%m/%d %H:%M:%S"
 
 
 def set_up_logging():
-    formatter = logging.Formatter(LOG_FORMAT)
-
     file_handler = logging.FileHandler(LOG_FILE, mode=LOG_FILE_MODE, encoding="utf-8")
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 
     stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(formatter)
+    stdout_handler.setFormatter(logging.Formatter(LOG_FILE_FORMAT, datefmt=LOG_DATE_FORMAT))
 
     logger = logging.getLogger(__package__)
     logger.setLevel(level=logging.DEBUG)
