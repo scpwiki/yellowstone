78cdc442ad445af315733640f1cc43f79fb49336
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-13 00:44:34 -0500

    Add more columns?

--- migrations/20240112_01_Njk7j-create-initial-tables.sql
+++ migrations/20240112_01_Njk7j-create-initial-tables.sql
@@ -22,12 +22,28 @@ CREATE TABLE page (
     site_slug TEXT FOREIGN KEY site(site_slug),
     page_slug TEXT NOT NULL,
     page_id INTEGER,
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
     deleted_at TIMESTAMP WITH TIME ZONE,
+    created_by TEXT,
+    updated_by TEXT,
+    title TEXT,
+    parent_slug TEXT,
+    parent_slug_fetched BOOLEAN DEFAULT false,
+    tags TEXT[],
     wikitext BYTEA,
     html BYTEA,
     page_category_id INTEGER,
     discussion_thread_id INTEGER,
     discussion_thread_fetched BOOLEAN DEFAULT false,
+    rating REAL,
+    comments INTEGER,
 
     UNIQUE (site_slug, page_slug, deleted_at)
+);
+
+CREATE TABLE job (
+    job_id SERIAL PRIMARY KEY,
+    job_type TEXT NOT NULL,
+    data JSON NOT NULL
 );
