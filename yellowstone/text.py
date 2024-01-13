6f5e2923191ff174ba3c440e13397aaf60c116ab
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-12 23:10:37 -0500

    Add module showing how to hash text.

---
+++ yellowstone/text.py
@@ -0,0 +1,8 @@
+# TODO demo of text hashing
+
+from Crypto.Hash import KangarooTwelve
+
+def hash_text(value: str) -> bytes:
+    h = KangarooTwelve.new()
+    h.update(value.encode("utf-8"))
+    return h.read(16)
