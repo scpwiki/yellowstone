d5be52c8fdf9f99bf1bbc6ee086e00f7914ae843
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-12 23:40:59 -0500

    Add data to database using new queries.

--- yellowstone/jobs/site_home_raw.py
+++ yellowstone/jobs/site_home_raw.py
@@ -3,43 +3,37 @@ Scrape site and page data from the home page of a site.
 """
 
 import re
-from dataclasses import dataclass
 
 from ..utils import download_html, regex_extract
 
-DOMAIN_REGEX = re.compile(r'WIKIREQUEST\.info\.domain = "([^"]+)";')
 LANGUAGE_REGEX = re.compile(r"WIKIREQUEST\.info\.lang = '([^']+)';")
 SITE_ID_REGEX = re.compile(r"WIKIREQUEST\.info\.siteId = (\d+);")
 SITE_SLUG_REGEX = re.compile(r'WIKIREQUEST\.info\.siteUnixName = "([^"]+)";')
 PAGE_ID_REGEX = re.compile(r"WIKIREQUEST\.info\.pageId = (\d+);")
 PAGE_SLUG_REGEX = re.compile(r'WIKIREQUEST\.info\.pageUnixName = "([^"]+)";')
+PAGE_CATEGORY_ID_REGEX = re.compile(r"WIKIREQUEST\.info\.categoryId = (\d+);")
 
 
-@dataclass
-class SiteHomeRaw:
-    domain: str
-    language: str
-    site_slug: str
-    site_id: int
-    home_page_slug: str
-    home_page_id: int
-
-
-def fetch(*, site_slug: str) -> SiteHomeRaw:
+def fetch(*, database, site_slug: str) -> SiteHomeRaw:
     url = f"https://{site_slug}.wikidot.com/"
     html = download_html(url)
-    domain = regex_extract(url, html, DOMAIN_REGEX)[1]
     language = regex_extract(url, html, LANGUAGE_REGEX)[1]
     site_id = int(regex_extract(url, html, SITE_ID_REGEX)[1])
     site_slug_ex = regex_extract(url, html, SITE_SLUG_REGEX)[1]
     page_id = int(regex_extract(url, html, PAGE_ID_REGEX)[1])
     page_slug = regex_extract(url, html, PAGE_SLUG_REGEX)[1]
+    page_category_id = int(regex_extract(url, html, PAGE_CATEGORY_ID_REGEX)[1])
     assert site_slug == site_slug_ex, "site slug in scraped page doesn't match"
-    return SiteHomeRaw(
-        domain=domain,
-        language=language,
-        site_id=site_id,
+
+    database.add_site(
         site_slug=site_slug,
-        home_page_id=page_id,
-        home_page_slug=page_slug,
+        wikidot_id=site_id,
+        home_slug=page_slug,
+        language=language,
+    )
+    database.add_page(
+        site_slug=site_slug,
+        page_slug=page_slug,
+        page_id=page_id,
+        page_category_id=page_category_id,
     )
