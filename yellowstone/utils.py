9341917bd3f746ea25908cdea9665e0424366fb5
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-14 22:26:06 -0500

    Add chunks() helper function.

---
+++ yellowstone/utils.py
@@ -0,0 +1,14 @@
+"""
+Miscellaneous utility functions.
+"""
+
+from itertools import islice
+from typing import Iterable, TypeVar
+
+T = TypeVar("T")
+
+
+def chunks(it: Iterable[T], size: int) -> Iterable[tuple[T]]:
+    iterator = iter(it)
+    while chunk := tuple(islice(iterator, size)):
+        yield chunk
