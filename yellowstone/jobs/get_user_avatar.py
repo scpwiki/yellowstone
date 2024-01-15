ec2e76234a63d2268af857c3fd34432abafaba49
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-15 05:12:14 -0500

    Remove unnecessary check.

--- yellowstone/jobs/get_user_avatar.py
+++ yellowstone/jobs/get_user_avatar.py
@@ -5,7 +5,6 @@ Stores the current avatar for a user.
 import logging
 from typing import TYPE_CHECKING
 
-from ..exceptions import JobFailed
 from ..requests import user_avatar
 
 if TYPE_CHECKING:
@@ -15,12 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 def run(core: "BackupDispatcher", *, user_slug: str, user_id: int) -> None:
-    logging.info("Downloading avatar for user '%s' (%d)", user_slug, user_id)
-    user = core.database.get_user_by_id(user_id=user_id)
-    if user is None:
-        logging.error("No user found with this ID, marking as deleted")
-        raise JobFailed
-
+    logger.info("Downloading avatar for user '%s' (%d)", user_slug, user_id)
     avatar = user_avatar.get(user_id)
     hash = core.s3.upload_avatar(avatar)
     core.database.add_user_avatar(hash=hash, user_id=user_id)
