ae6cc57aa9f7bda96c4aff8c8ff056a6c2f5db3c
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-15 04:47:00 -0500

    Do nothing if member record is already present.

--- queries/add_site_member.sql
+++ queries/add_site_member.sql
@@ -1,3 +1,5 @@
 -- :name add_site_member :insert
 INSERT INTO site_member (user_id, site_id, joined_at)
     VALUES (:user_id, :site_id, :joined_at)
+    ON CONFLICT (user_id, site_id)
+    DO NOTHING
