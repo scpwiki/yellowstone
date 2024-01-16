51ba32e01825ab2e272eb33c18eddd2d0c5bb290
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-16 16:30:08 -0500

    Transform to merged-style query file for site_member.

--- queries/site_member.sql
+++ queries/site_member.sql
@@ -1,5 +1,7 @@
+-- Adds a site member relation, if not already present
+-- This prefers earlier join dates to later ones, if multiple exist
 -- :name add_site_member :insert
 INSERT INTO site_member (user_id, site_id, joined_at)
     VALUES (:user_id, :site_id, :joined_at)
     ON CONFLICT (user_id, site_id)
-    DO NOTHING
+    DO NOTHING;
