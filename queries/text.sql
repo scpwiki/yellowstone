ec747f5ec467500647755911c4e07053ac3d9480
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-16 16:27:08 -0500

    Transform to merged-style query file for text.

--- queries/text.sql
+++ queries/text.sql
@@ -1,5 +1,6 @@
+-- Inserts a text entry, if not already present
 -- :name add_text :insert
 INSERT INTO text (hash, contents)
     VALUES (:hash, :contents)
     ON CONFLICT (hash)
-    DO NOTHING
+    DO NOTHING;
