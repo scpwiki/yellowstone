a71252b15943d56ae606b62b8a314fa1e1dcf1bb
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-28 02:21:40 -0500

    Remove unnecessary RETURNING clause.

--- queries/forum.sql
+++ queries/forum.sql
@@ -1,7 +1,6 @@
 -- :name add_forum_group :insert
 INSERT INTO forum_group (site_slug, name, description)
-    VALUES (:site_slug, :name, :description)
-    RETURNING internal_id;
+    VALUES (:site_slug, :name, :description);
 
 -- :name add_forum_category :insert
 INSERT INTO forum_category (forum_category_id, forum_group_internal_id, name, description)
