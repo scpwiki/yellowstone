936033056d7df8a3648d4b573e63efb62efa9125
Emmie Maeda <emmie.maeda@gmail.com>
2024-03-07 06:10:41 -0500

    Add tests for utils.py

---
+++ test/test_utils.py
@@ -0,0 +1,19 @@
+import unittest
+
+from yellowstone.utils import chunks, sql_array
+
+
+class TestUtils(unittest.TestCase):
+    def test_chunks(self):
+        self.assertEqual(
+            list(chunks(range(10), 2)),
+            [(0, 1), (2, 3), (4, 5), (6, 7), (8, 9)],
+        )
+
+        self.assertEqual(
+            list(chunks("abcdef", 3)),
+            [("a", "b", "c"), ("d", "e", "f")],
+        )
+
+    def test_sql_array(self):
+        self.assertEqual(sql_array((3, 7, 9, 11)), "{3, 7, 9, 11}")
