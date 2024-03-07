caa2849b2830e7a8f31c18ac57280746a799257e
Emmie Maeda <emmie.maeda@gmail.com>
2024-03-07 02:07:23 -0500

    Add other two regex extract tests.

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,7 +1,7 @@
 import re
 import unittest
 
-from yellowstone.scraper import regex_extract
+from yellowstone.scraper import regex_extract, regex_extract_str, regex_extract_int
 
 
 class TestScraper(unittest.TestCase):
@@ -15,3 +15,13 @@ class TestScraper(unittest.TestCase):
         match = regex_extract("test", "SCP-371-J", regex)
         self.assertEqual(match[1], "371")
         self.assertEqual(match[2], "J")
+
+    def test_regex_extract_str(self):
+        regex = re.compile(r"name_(\w+)")
+        result = regex_extract_str("test", "name_j12pmu", regex)
+        self.assertEqual(result, "j12pmu")
+
+    def test_regex_extract_int(self):
+        regex = re.compile(r"id_(\d+)")
+        result = regex_extract_int("test", "id_9406112", regex)
+        self.assertEqual(result, 9406112)
